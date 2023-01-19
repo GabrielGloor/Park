867063799bedc613cf0bd6a6259557e585988cff
Gabriel.GLOOR <Gabriel.GLOOR@SC-C315-PC07>
2023-01-19 08:48:31 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace park
+{
+    public class Car
+    {
+        #region private attributes
+        private double _size;
+        #endregion private attributes
+
+        #region public methods
+        public Car(double size)
+        {
+
+        }
+         
+        public double Size
+        {
+            get 
+            { 
+                throw new NotImplementedException();
+            }
+        }
+        #endregion public methods
+    }
+}
---
+++ ParkingLot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace park
+{
+    public class ParkingLot
+    {
+        #region private attributes
+        private double _capacity;
+        private double _vacancy;
+        #endregion private attributes
+
+        #region public methods
+        public ParkingLot (double capacity)
+        {
+
+        }
+
+        public void Park(Car car)
+        {
+
+        }
+
+        public double Vacancy
+        {
+            get 
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+        #endregion public methods
+    }
+}
